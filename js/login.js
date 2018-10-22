51572942470fb731f85b8602aa22b1d3f360acc9
jero77 <34156935+jero77@users.noreply.github.com>
2018-10-22 10:29:37 +0200

    test authentication

--- js/login.js
+++ js/login.js
@@ -1,26 +1,19 @@
-var attempts = 3; //max. attempts
 /**
   This function validates the input of the loginform from "index.html".
   Executes on click on the button named "Login"
   @see{@link index.html}
 */
-function validate () {
+function validate (username, password) {
   //Get form input
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
   //try to validate it
-  if (username == "guestuser" && password == "noadmin") {
+  if (username == "guest" && password == "guest") {
     alert("Login was successful!");
-    window.location = "authenticated.html";
-    return false;
+    alert("Authenticated")
+    //window.location = "authenticated.html";
   } else {
-    attempts--;
-    alert("Wrong username and password! " + attempts + " attempts left.");
-    if (attempts == 0) {
-      document.getElementById("username").disabled = true;
-      document.getElementById("password").disabled = true;
-      document.getElementById("loginbutton").disabled = true;
-    }
+    alert("Wrong username and password!");
   }
 }
